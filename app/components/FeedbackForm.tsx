fb0ca719ff40c869a0b6defad0b709f6751a31d5
Stefano Casasola <ste.casasola@gmail.com>
2025-03-16 20:23:40 +0100

    Fixes a label

--- app/components/FeedbackForm.tsx
+++ app/components/FeedbackForm.tsx
@@ -253,7 +253,7 @@ const FeedbackForm = ({ events, selectedEvent }: FeedbackFormProps) => (
         type="submit"
         className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
       >
-        Save
+        Send
       </button>
     </div>
   </Form>
