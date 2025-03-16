ac55a1920b9f6dacd827aed69766540b7b8cdcd8
Stefano Casasola <ste.casasola@gmail.com>
2025-03-16 15:42:15 +0100

    Hotfix

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,16 +1,16 @@
 const Navbar = () => (
-  <nav className="bg-white border-gray-200">
+  <nav className="border-gray-200 bg-gray-900">
     <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
         <img src="logo.png" className="h-8" alt="Feedback Hug Logo" />
-        <span className="self-center text-2xl font-semibold whitespace-nowrap">
+        <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
           Feedback Hub
         </span>
       </a>
       <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
         <button
           type="button"
-          className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300"
+          className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 focus:ring-gray-600"
           id="user-menu-button"
           aria-expanded="false"
           data-dropdown-toggle="user-dropdown"
